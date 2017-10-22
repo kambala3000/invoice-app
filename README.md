c57b6dcd2c1cf2e8a654909bcab487b44b7ede7e
kambala3000 <kambala02@gmail.com>
2017-10-22 21:10:28 +0300

    buld config

--- README.md
+++ README.md
@@ -6,3 +6,7 @@
 <code>npm start</code>
 </pre>
 <p>Then open <a href="http://localhost:8000/">http://localhost:8000/</a> to see app.</p>
+<p>To build: </p>
+<pre>
+<code>npm run build</code>
+</pre>
---
+++ webpack.production.config.js
@@ -0,0 +1,48 @@
+'use strict';
+
+var path = require('path');
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    devtool: 'eval-source-map',
+    entry: [path.join(__dirname, 'src/index.jsx')],
+    resolve: {
+        root: [path.resolve(__dirname, 'src')],
+        extensions: ['', '.js', '.jsx', '.css']
+    },
+    output: {
+        path: path.join(__dirname, '/public/'),
+        filename: 'index.bundle.js',
+        publicPath: ''
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'src/index.tpl.html',
+            inject: 'body',
+            filename: 'index.html'
+        }),
+        new webpack.optimize.OccurenceOrderPlugin(),
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoErrorsPlugin(),
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': JSON.stringify('production')
+        })
+    ],
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015', 'react', 'stage-0']
+                }
+            },
+            {
+                test: /\.css$/,
+                loader: 'style!css'
+            }
+        ]
+    }
+};
